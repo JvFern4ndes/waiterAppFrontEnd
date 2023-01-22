a5d5844d43b266fe6e45efc655dd0ca7f3a48d22
João Victor <jvfernandes.contato@gmail.com>
2023-01-22 04:00:23 -0300

    feat: creation of Orders component

---
+++ fe/src/components/Orders/index.tsx
@@ -0,0 +1,64 @@
+import { Container, Board, OrdersContainer } from './styles';
+
+export function Orders() {
+  return (
+    <Container>
+      <Board>
+        <header>
+          <span>🕒️</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+
+      <Board>
+        <header>
+          <span>🕒️</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+
+      <Board>
+        <header>
+          <span>🕒️</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </Container>
+  );
+}
