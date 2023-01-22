cb3ab317d76d8c818a2d71f589edeab1ad36dc1c
João Victor <jvfernandes.contato@gmail.com>
2023-01-22 04:22:03 -0300

    feat: refactoring the orders model with the creation of the OrdersBoard model

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,64 +1,23 @@
-import { Container, Board, OrdersContainer } from './styles';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container } from './styles';
 
 export function Orders() {
   return (
     <Container>
-      <Board>
-        <header>
-          <span>🕒️</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
+      <OrdersBoard
+        icon="🕒️"
+        title="Fila de espera"
+      />
 
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard
+        icon="🧑‍🍳️"
+        title="Em preparação"
+      />
 
-      <Board>
-        <header>
-          <span>🕒️</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-
-      <Board>
-        <header>
-          <span>🕒️</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard
+        icon="✅️"
+        title="Pronto!"
+      />
     </Container>
   );
 }
