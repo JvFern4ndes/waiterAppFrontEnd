7567c1d411ebb939e619c85d8ce5daf320562d31
João Victor <jvfernandes.contato@gmail.com>
2023-01-23 19:02:03 -0300

    feat: creation of orderModal

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,5 +1,34 @@
+import { Order } from '../../types/Order';
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './styles';
+
+const orders: Order[] = [
+  {
+    '_id': '63cafc78575089323eb22e04',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1674245010478-quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '63cafc78575089323eb22e05'
+      },
+      {
+        'product': {
+          'name': 'Coca cola',
+          'imagePath': '1674246191131-coca-cola.png',
+          'price': 7,
+        },
+        'quantity': 2,
+        '_id': '63cafc78575089323eb22e06'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
@@ -7,16 +36,19 @@ export function Orders() {
       <OrdersBoard
         icon="🕒️"
         title="Fila de espera"
+        orders={orders}
       />
 
       <OrdersBoard
         icon="🧑‍🍳️"
         title="Em preparação"
+        orders={[]}
       />
 
       <OrdersBoard
         icon="✅️"
         title="Pronto!"
+        orders={[]}
       />
     </Container>
   );
